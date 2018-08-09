39635f00ba1319e7ca9d39e6d46c7d793aea9064
unknown <shirley.mramirez@yahoo.com>
2018-08-09 11:37:41 -0700

    applying logical &&

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -3,35 +3,31 @@ import React, { Component } from "react";
 class Counter extends Component {
   state = {
     count: 0,
-    tags: ["tag1", "tag2", "tag3"]
+    tags: []
   };
+
+  // rendering method
+  // will return an empty tags or a bunch of tags
+  renderTags() {
+    if (this.state.tags.length === 0) return <p>There are no tags!</p>;
+
+    return (
+      <ul>
+        {this.state.tags.map(tag => (
+          <li key={tag}>{tag}</li>
+        ))}
+      </ul>
+    );
+  }
 
   render() {
     return (
       <div>
-        <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
-        <button className="btn btn-secondary btn-sm">Increment</button>
-        {/* for rendering lists of tags */}
-        <ul>
-          {/* map method of arrays, to map each array into react elements  */}
-          {/* getting a string and mapping it a plain javascript expresion */}
-          {this.state.tags.map(tag => (
-            <li key={tag}>{tag}</li>
-          ))}
-        </ul>
+        {/* applying logical 'and' between a bolean value and a string */}
+        {this.state.tags.length === 0 && "Please create a new tag!"}
+        {this.renderTags()}
       </div>
     );
-  }
-
-  getBadgeClasses() {
-    let classes = "badge m-2 badge-";
-    classes += this.state.count === 0 ? "warning" : "primary";
-    return classes;
-  }
-
-  formatCount() {
-    const { count } = this.state;
-    return count === 0 ? "Zero" : count;
   }
 }
 
