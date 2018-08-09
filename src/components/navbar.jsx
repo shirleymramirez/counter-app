a2f2b1f5439767b2d24d677c40448c3d5d3e77ed
unknown <shirley.mramirez@yahoo.com>
2018-08-09 14:24:08 -0700

    added navbar

---
+++ src/components/navbar.jsx
@@ -0,0 +1,15 @@
+import React, { Component } from "react";
+
+class Navbar extends Component {
+  render() {
+    return (
+      <nav class="navbar navbar-light bg-light">
+        <a class="navbar-brand" href="#">
+          Navbar
+        </a>
+      </nav>
+    );
+  }
+}
+
+export default Navbar;
