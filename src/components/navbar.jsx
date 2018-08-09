3cf2565fb7d7637ea33f6559e7a12186b57df1c7
unknown <shirley.mramirez@yahoo.com>
2018-08-09 14:38:34 -0700

    changed nabvar.js from class components into stateless functional components

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,18 +1,17 @@
 import React, { Component } from "react";
 
-class Navbar extends Component {
-  render() {
-    return (
-      <nav class="navbar navbar-light bg-light">
-        <a class="navbar-brand" href="#">
-          Navbar{" "}
-          <span className="badge badge-pill badge-secondary">
-            {this.props.totalCounters}
-          </span>
-        </a>
-      </nav>
-    );
-  }
-}
+// Stateless Functional Component
+const NavBar = props => {
+  return (
+    <nav class="navbar navbar-light bg-light">
+      <a class="navbar-brand" href="#">
+        Navbar{" "}
+        <span className="badge badge-pill badge-secondary">
+          {props.totalCounters}
+        </span>
+      </a>
+    </nav>
+  );
+};
 
 export default Navbar;
